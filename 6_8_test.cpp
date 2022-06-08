e57ea74379511b25d1804ee3f72544eb61cd1936
MMDyuchen <103278041+MMDyuchen@users.noreply.github.com>
2022-06-08 23:05:45 +0800

    Create 6_8_test.cpp
    数据存储最后一节，关于浮点整形之间的转换还有问题

---
+++ 6_8_test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARMINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+#include <stdlib.h>
+int main()
+{
+	float f = 5.5;
+	//5.5
+	//101.1
+	//(-1)^0 * 1.011 * 2^2
+	//S = 0
+	//M = 1.011
+	//E = 2
+	//0100 0000 1011 0000 0000 0000 0000 0000
+	//0x40b00000
+	return 0;
+}
