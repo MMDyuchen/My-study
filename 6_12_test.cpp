2ad1bb77116d1a2a8cfc1d71bd77317d3a509068
MMDyuchen <103278041+MMDyuchen@users.noreply.github.com>
2022-06-12 23:29:57 +0800

    Create 6_12_test.cpp
    数组的再次回顾
    数组存储的指针位置

---
+++ 6_12_test.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARMINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+#include <stdlib.h>
+
+//int main(void)
+//{
+//	int number = 10;
+//	int count = 10;
+//	long sum = 0L;
+//	float average = 0.0f;
+//	for (int i = 0; i < count; i++)
+//	{
+//		printf("Enter grade: ");
+//		scanf_s("%d ", &number);
+//		sum += number;
+//	}
+//	average = (float)sum / count;
+//	printf("\nAverage of the ten numbers entered is :%f\n", average);
+//	return 0;
+//}
+//有多少名学生，count和number就等于多少
+
+////数组表示
+//int main()
+//{
+//	int arr[10];
+//	int count = 10;
+//	long sum = 0L;
+//	float average = 0.0f;
+//	printf("\nEnter the 10 numbers:\n");
+//	for(int i = 0; i < count; i++)
+//	{
+//		printf("%2d> ",i+1 );
+//		scanf_s("%d ", &arr[i]);
+//		sum += arr[i];
+//	}
+//	average = (float)sum / count;
+//	for (int i = 0; i < count; i++)
+//	{
+//		printf("the number %d grades was%d \n",i + 1,arr[i]);
+//	}
+//	printf("\nAverage of the ten numbers entered is :%f\n", average);
+//	return 0;
+//}
+
+
