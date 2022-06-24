afea37367c3be48dc002fa4e623124123f4df334
MMDyuchen <103278041+MMDyuchen@users.noreply.github.com>
2022-06-24 23:43:46 +0800

    Create 6_24_test.cpp
    指针详解

---
+++ 6_24_test.cpp
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARMINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+#include <stdlib.h>
+//int main()
+//{
+//	 
+//	return 0;
+//}
+int Add(int x, int y)
+{
+	return x + y;
+}
+
+int main()
+{
+	printf()"hehe\n";
+	int a = 20;
+	int b = 10;
+	int c = Add(a, b);
+	return 0;
+}
+
+int main()
+{
+	int arr[10] = { 0 };
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		arr[i] = i;
+	}
+	return 0;
+}
+
+int main()
+{
+	{
+		int tmo = 0;
+		printf("tmp = %d\n", tmp);
+	}
+	int arr[10] = { 0 };
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		arr[i] = i;
+	}
+	return 0;
+}
+
+void test2()
+{
+	printf("hehe\n");
+}
+void test1()
+{
+	test2();
+}
+void test()
+{
+	tset1();
+}
+int main()
+{
+	test();
+	return 0;
+}
+
+
+300 * 4 * 5 = 6000
+400 * 5 * 5 = 10000
+
